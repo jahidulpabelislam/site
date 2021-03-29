a7c5b6674179d44eaefda15c261731e573d72b25
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2021-03-29 19:22:35 +0100

    Remove import

--- assets/favicons.php
+++ assets/favicons.php
@@ -1,26 +1,24 @@
 <?php
-use JPI\Site;
-
 $root = __DIR__ . "/..";
 ?>
 
-<link rel="apple-touch-icon" sizes="57x57" href="<?php echo Site::asset("/assets/favicons/apple-touch-icon-57x57.png", null, $root); ?>" />
-<link rel="apple-touch-icon" sizes="60x60" href="<?php echo Site::asset("/assets/favicons/apple-touch-icon-60x60.png", null, $root); ?>" />
-<link rel="apple-touch-icon" sizes="72x72" href="<?php echo Site::asset("/assets/favicons/apple-touch-icon-72x72.png", null, $root); ?>" />
-<link rel="apple-touch-icon" sizes="76x76" href="<?php echo Site::asset("/assets/favicons/apple-touch-icon-76x76.png", null, $root); ?>" />
-<link rel="apple-touch-icon" sizes="114x114" href="<?php echo Site::asset("/assets/favicons/apple-touch-icon-114x114.png", null, $root); ?>" />
-<link rel="apple-touch-icon" sizes="120x120" href="<?php echo Site::asset("/assets/favicons/apple-touch-icon-120x120.png", null, $root); ?>" />
-<link rel="apple-touch-icon" sizes="144x144" href="<?php echo Site::asset("/assets/favicons/apple-touch-icon-144x144.png", null, $root); ?>" />
-<link rel="apple-touch-icon" sizes="152x152" href="<?php echo Site::asset("/assets/favicons/apple-touch-icon-152x152.png", null, $root); ?>" />
-<link rel="apple-touch-icon" sizes="180x180" href="<?php echo Site::asset("/assets/favicons/apple-touch-icon-180x180.png", null, $root); ?>" />
-<link rel="icon" type="image/png" sizes="32x32" href="<?php echo Site::asset("/assets/favicons/favicon-32x32.png", null, $root); ?>" />
-<link rel="icon" type="image/png" sizes="194x194" href="<?php echo Site::asset("/assets/favicons/favicon-194x194.png", null, $root); ?>" />
-<link rel="icon" type="image/png" sizes="192x192" href="<?php echo Site::asset("/assets/favicons/android-chrome-192x192.png", null, $root); ?>" />
-<link rel="icon" type="image/png" sizes="16x16" href="<?php echo Site::asset("/assets/favicons/favicon-16x16.png", null, $root); ?>" />
-<link rel="manifest" href="<?php echo Site::asset("/assets/favicons/site.webmanifest", null, $root); ?>" />
-<link rel="mask-icon" href="<?php echo Site::asset("/assets/favicons/safari-pinned-tab.svg", null, $root); ?>" color="<?php echo Site::COLOUR ?>" />
-<link rel="shortcut icon" href="<?php echo Site::asset("/favicon.ico", null, $root); ?>" />
-<meta name="msapplication-TileColor" content="<?php echo Site::COLOUR ?>" />
-<meta name="msapplication-TileImage" content="<?php echo Site::asset("/assets/favicons/mstile-144x144.png", null, $root); ?>" />
-<meta name="msapplication-config" content="<?php echo Site::asset("/assets/favicons/browserconfig.xml", null, $root); ?>" />
+<link rel="apple-touch-icon" sizes="57x57" href="<?php echo self::asset("/assets/favicons/apple-touch-icon-57x57.png", null, $root); ?>" />
+<link rel="apple-touch-icon" sizes="60x60" href="<?php echo self::asset("/assets/favicons/apple-touch-icon-60x60.png", null, $root); ?>" />
+<link rel="apple-touch-icon" sizes="72x72" href="<?php echo self::asset("/assets/favicons/apple-touch-icon-72x72.png", null, $root); ?>" />
+<link rel="apple-touch-icon" sizes="76x76" href="<?php echo self::asset("/assets/favicons/apple-touch-icon-76x76.png", null, $root); ?>" />
+<link rel="apple-touch-icon" sizes="114x114" href="<?php echo self::asset("/assets/favicons/apple-touch-icon-114x114.png", null, $root); ?>" />
+<link rel="apple-touch-icon" sizes="120x120" href="<?php echo self::asset("/assets/favicons/apple-touch-icon-120x120.png", null, $root); ?>" />
+<link rel="apple-touch-icon" sizes="144x144" href="<?php echo self::asset("/assets/favicons/apple-touch-icon-144x144.png", null, $root); ?>" />
+<link rel="apple-touch-icon" sizes="152x152" href="<?php echo self::asset("/assets/favicons/apple-touch-icon-152x152.png", null, $root); ?>" />
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo self::asset("/assets/favicons/apple-touch-icon-180x180.png", null, $root); ?>" />
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo self::asset("/assets/favicons/favicon-32x32.png", null, $root); ?>" />
+<link rel="icon" type="image/png" sizes="194x194" href="<?php echo self::asset("/assets/favicons/favicon-194x194.png", null, $root); ?>" />
+<link rel="icon" type="image/png" sizes="192x192" href="<?php echo self::asset("/assets/favicons/android-chrome-192x192.png", null, $root); ?>" />
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo self::asset("/assets/favicons/favicon-16x16.png", null, $root); ?>" />
+<link rel="manifest" href="<?php echo self::asset("/assets/favicons/site.webmanifest", null, $root); ?>" />
+<link rel="mask-icon" href="<?php echo self::asset("/assets/favicons/safari-pinned-tab.svg", null, $root); ?>" color="<?php echo self::COLOUR ?>" />
+<link rel="shortcut icon" href="<?php echo self::asset("/favicon.ico", null, $root); ?>" />
+<meta name="msapplication-TileColor" content="<?php echo self::COLOUR ?>" />
+<meta name="msapplication-TileImage" content="<?php echo self::asset("/assets/favicons/mstile-144x144.png", null, $root); ?>" />
+<meta name="msapplication-config" content="<?php echo self::asset("/assets/favicons/browserconfig.xml", null, $root); ?>" />
 <meta name="theme-color" content="#f5f5f5" />
