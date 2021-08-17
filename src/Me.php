a58f01a5c5e92a04ce1fa40e02627b19605c9b3a
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2021-08-17 17:27:46 +0100

    2012 start date

--- src/Me.php
+++ src/Me.php
@@ -3,17 +3,15 @@
 namespace JPI;
 
 interface Me {
-
     public const NAME = "Jahidul Pabel Islam";
 
     public const DATE_OF_BIRTH = "1996-02-22";
 
     public const JOB = "Full Stack Developer";
 
-    public const START_DATE = "2010-10-04";
+    public const START_DATE = "2012-09-17";
 
     public const SOCIAL_GITHUB = "jahidulpabelislam";
     public const SOCIAL_INSTAGRAM = "jahidulpabelislam";
     public const SOCIAL_LINKEDIN = "jahidulpabelislam";
-
 }
