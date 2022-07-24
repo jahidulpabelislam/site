dacc372164c7168a76f6de2e93949afa70f3d25b
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2022-07-24 16:57:39 +0100

    Add professional start date

--- src/Me.php
+++ src/Me.php
@@ -6,11 +6,12 @@ interface Me {
 
     public const NAME = "Jahidul Pabel Islam";
 
-    public const DATE_OF_BIRTH = "1996-02-22";
-
     public const JOB = "Full Stack Developer";
 
+    public const DATE_OF_BIRTH = "1996-02-22";
+
     public const START_DATE = "2012-09-17";
+    public const PROFESSIONAL_START_DATE = "2017-06-28";
 
     public const SOCIAL_GITHUB = "jahidulpabelislam";
     public const SOCIAL_INSTAGRAM = "jahidulpabelislam";
